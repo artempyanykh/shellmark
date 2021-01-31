27a86f18d21f87e7f2bd9bb264ce67fae3da1188
Artem Pyanykh <artem.pyanykh@gmail.com>
2021-01-31 16:36:31 +0000

    Render a bookmark table with selection and highlighting

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,27 @@
 use crossterm::{
-    cursor,
     event::{Event, EventStream, KeyCode, KeyEvent, KeyModifiers},
-    execute, terminal, ExecutableCommand,
+    execute, terminal,
 };
-use cursor::{MoveDown, MoveRight};
 use futures::stream::{self, TryStreamExt};
-use io::{stderr, stdout, Write};
 use std::{
     error::Error,
     io::{self, Stdout},
     iter::FromIterator,
-    pin::Pin,
+    path::PathBuf,
     process::exit,
+    str::FromStr,
     time::Duration,
+    unreachable, usize,
 };
+use terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use tokio::time::Instant;
 use tokio_stream::StreamExt;
 use tui::{
     backend::CrosstermBackend,
-    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
     text::Span,
-    widgets::{Block, Borders, Clear, Paragraph},
+    widgets::{Block, Borders, Cell, Paragraph, Row, Table, TableState},
     Terminal,
 };
 
@@ -48,19 +50,130 @@ impl From<Tick> for SystemEvent {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Bookmark {
+    name: String,
+    dest: PathBuf,
+}
+
+impl Bookmark {
+    pub fn new(name: String, dest: PathBuf) -> Bookmark {
+        Bookmark { name, dest }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+struct InputState {
+    input: Vec<char>,
+    cursor: u16,
+}
+
+impl InputState {
+    fn insert_char(&self, c: char) -> Self {
+        let mut new_state = self.clone();
+        new_state.input.insert(new_state.cursor as usize, c);
+        new_state.cursor += 1;
+        new_state
+    }
+
+    fn delete_char_backwards(&self) -> Self {
+        let mut new_state = self.clone();
+        if new_state.input.is_empty() {
+            return new_state;
+        }
+
+        new_state.input.remove((new_state.cursor - 1) as usize);
+        new_state.cursor -= 1;
+
+        new_state
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct SelectionState {
+    selection: Vec<Bookmark>,
+    highlight: Option<usize>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum MoveDirection {
+    Down,
+    Up,
+}
+
+impl MoveDirection {
+    fn increment(&self) -> i8 {
+        match self {
+            MoveDirection::Down => 1,
+            MoveDirection::Up => -1,
+        }
+    }
+}
+
+impl SelectionState {
+    fn new(selection: Vec<Bookmark>, highlight: Option<usize>) -> Self {
+        Self {
+            selection,
+            highlight,
+        }
+    }
+
+    fn move_highlight(&self, direction: MoveDirection) -> Self {
+        if self.selection.is_empty() {
+            return self.clone();
+        }
+        match self.highlight {
+            None => SelectionState {
+                highlight: Some(0),
+                ..self.clone()
+            },
+            Some(line) => {
+                let increment = direction.increment();
+                let new_line = (line as isize + increment as isize)
+                    .max(0)
+                    .min(self.selection.len() as isize - 1) as usize;
+
+                SelectionState {
+                    highlight: Some(new_line),
+                    ..self.clone()
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct CursorLoc {
+    x: u16,
+    y: u16,
+}
+
+impl CursorLoc {
+    fn new(x: u16, y: u16) -> Self {
+        Self { x, y }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct AppState {
-    user_input: Vec<char>,
-    cursor_pos: u16,
-    // We'll use this later, to play more nicely with pre-existing content
-    initial_cursor: (u16, u16),
+    input_state: InputState,
+    selection_state: SelectionState,
+    bookmarks: Vec<Bookmark>,
+    last_refresh_at: Option<Instant>,
 }
 
 impl AppState {
-    fn new(x: u16, y: u16) -> AppState {
+    pub fn new(bookmarks: Vec<Bookmark>) -> AppState {
+        let input_state = InputState::default();
+        let selection_state = SelectionState::new(
+            bookmarks.clone(),
+            if bookmarks.is_empty() { None } else { Some(0) },
+        );
         AppState {
-            user_input: Vec::new(),
-            cursor_pos: 0,
-            initial_cursor: (x, y),
+            input_state,
+            selection_state,
+            bookmarks,
+            last_refresh_at: None,
         }
     }
 }
@@ -68,15 +181,23 @@ impl AppState {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     terminal::enable_raw_mode()?;
-    let stdout = io::stdout();
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+
+    let dummy_bookmarks = vec![
+        Bookmark::new("A".into(), PathBuf::from_str("dest/a")?),
+        Bookmark::new("B".into(), PathBuf::from_str("dest/b")?),
+        Bookmark::new("C".into(), PathBuf::from_str("dest/c")?),
+        Bookmark::new("D".into(), PathBuf::from_str("dest/d")?),
+        Bookmark::new("E".into(), PathBuf::from_str("dest/e")?),
+        Bookmark::new("F".into(), PathBuf::from_str("dest/f")?),
+        Bookmark::new("G".into(), PathBuf::from_str("dest/g")?),
+        Bookmark::new("H".into(), PathBuf::from_str("dest/h")?),
+    ];
 
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
-    let initial_cursor = terminal.get_cursor()?;
-    let mut app_state = AppState::new(initial_cursor.0, initial_cursor.1);
-
-    // For now support only full-screen mode
-    terminal.clear()?;
+    let mut app_state = AppState::new(dummy_bookmarks);
 
     // Setup an event loop
     let ticks = stream::repeat(Tick)
@@ -92,20 +213,115 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .await?
             .expect("Ticks are always present");
 
-        main_loop(event, &mut app_state, &mut terminal).await?;
+        app_state = main_loop(event, app_state, &mut terminal).await?;
     }
 }
 
 async fn main_loop(
     event: SystemEvent,
-    app_state: &mut AppState,
+    app_state: AppState,
     terminal: &mut Terminal<CrosstermBackend<Stdout>>,
-) -> Result<(), Box<dyn Error>> {
-    match event {
-        SystemEvent::Timer(_) => (),
-        SystemEvent::User(Event::Key(k)) => handle_key_event(app_state, k, terminal),
-        _ => (),
+) -> Result<AppState, Box<dyn Error>> {
+    let (should_repaint, new_state) = match event {
+        SystemEvent::Timer(_) => match app_state.last_refresh_at {
+            None => (
+                true,
+                AppState {
+                    last_refresh_at: Instant::now().into(),
+                    ..app_state.clone()
+                },
+            ),
+            Some(_) => (false, app_state.clone()),
+        },
+        SystemEvent::User(Event::Key(k)) => {
+            let mut new_state = handle_key_event(&app_state, k);
+            if new_state != app_state {
+                new_state.last_refresh_at = Instant::now().into();
+                (true, new_state)
+            } else {
+                (false, new_state)
+            }
+        }
+        _ => (
+            true,
+            AppState {
+                last_refresh_at: Instant::now().into(),
+                ..app_state.clone()
+            },
+        ),
+    };
+
+    if should_repaint {
+        draw_ui(terminal, &new_state)?;
     }
+
+    Ok(new_state)
+}
+
+fn handle_key_event(app_state: &AppState, event: KeyEvent) -> AppState {
+    match event {
+        KeyEvent {
+            code: KeyCode::Char('c'),
+            modifiers: KeyModifiers::CONTROL,
+        } => {
+            exit_app();
+            unreachable!();
+        }
+        KeyEvent {
+            code: KeyCode::Char('n'),
+            modifiers: KeyModifiers::CONTROL,
+        }
+        | KeyEvent {
+            code: KeyCode::Down,
+            ..
+        } => AppState {
+            selection_state: app_state
+                .selection_state
+                .move_highlight(MoveDirection::Down),
+            ..app_state.clone()
+        },
+        KeyEvent {
+            code: KeyCode::Char('p'),
+            modifiers: KeyModifiers::CONTROL,
+        }
+        | KeyEvent {
+            code: KeyCode::Up, ..
+        } => AppState {
+            selection_state: app_state.selection_state.move_highlight(MoveDirection::Up),
+            ..app_state.clone()
+        },
+        KeyEvent {
+            code: KeyCode::Char(c),
+            ..
+        } => AppState {
+            input_state: app_state.input_state.insert_char(c),
+            ..app_state.clone()
+        },
+        KeyEvent {
+            code: KeyCode::Backspace,
+            modifiers,
+        } => {
+            if modifiers == KeyModifiers::CONTROL {
+                AppState {
+                    input_state: InputState::default(),
+                    ..app_state.clone()
+                }
+            } else {
+                AppState {
+                    input_state: app_state.input_state.delete_char_backwards(),
+                    ..app_state.clone()
+                }
+            }
+        }
+        _ => app_state.clone(),
+    }
+}
+
+fn draw_ui(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+    new_state: &AppState,
+) -> Result<(), Box<dyn Error>> {
+    let mut cursor_loc = CursorLoc::new(0, 0);
 
     terminal.draw(|f| {
         let all_area = f.size();
@@ -137,63 +353,47 @@ async fn main_loop(
         );
 
         f.render_widget(
-            Paragraph::new(Span::raw(String::from_iter(&app_state.user_input)))
+            Paragraph::new(Span::raw(String::from_iter(&new_state.input_state.input)))
                 .alignment(Alignment::Left),
             input_block_area,
         );
 
-        f.set_cursor(
-            input_block_area.x + app_state.cursor_pos,
+        let list_area = Layout::default()
+            .horizontal_margin(1)
+            .constraints([Constraint::Percentage(100)])
+            .split(chunks[1])[0];
+        let mut rows = Vec::with_capacity(new_state.selection_state.selection.len());
+        for bm in &new_state.selection_state.selection {
+            let bm_name = Cell::from(bm.name.as_ref()).style(Style::default().fg(Color::Green));
+            let bm_dest = Cell::from(bm.dest.to_string_lossy().to_string());
+            let row = Row::new(vec![bm_name, bm_dest]);
+            rows.push(row);
+        }
+        let bookmarks_tbl = Table::new(rows)
+            .block(Block::default())
+            .column_spacing(1)
+            .widths(&[Constraint::Min(10), Constraint::Min(10)])
+            .highlight_symbol(">> ")
+            .highlight_style(Style::default().add_modifier(Modifier::BOLD));
+        let mut bookmarks_state = TableState::default();
+        bookmarks_state.select(new_state.selection_state.highlight);
+
+        f.render_stateful_widget(bookmarks_tbl, list_area, &mut bookmarks_state);
+
+        cursor_loc = CursorLoc::new(
+            input_block_area.x + new_state.input_state.cursor,
             input_block_area.y,
         );
-
-        let list_area = chunks[1];
     })?;
+
+    terminal.set_cursor(cursor_loc.x, cursor_loc.y)?;
+    terminal.show_cursor()?;
 
     Ok(())
 }
 
-fn handle_key_event(
-    app_state: &mut AppState,
-    event: KeyEvent,
-    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
-) {
-    match event {
-        KeyEvent {
-            code: KeyCode::Char('c'),
-            modifiers: KeyModifiers::CONTROL,
-        } => exit_app(terminal),
-        KeyEvent {
-            code: KeyCode::Char(c),
-            ..
-        } => {
-            app_state.user_input.insert(app_state.cursor_pos.into(), c);
-            app_state.cursor_pos += 1;
-        }
-        KeyEvent {
-            code: KeyCode::Backspace,
-            modifiers,
-        } => {
-            if modifiers == KeyModifiers::CONTROL {
-                app_state.user_input.clear();
-                app_state.cursor_pos = 0;
-            } else {
-                if app_state.cursor_pos > 0 {
-                    app_state
-                        .user_input
-                        .remove((app_state.cursor_pos - 1).into());
-                    app_state.cursor_pos -= 1;
-                }
-            }
-        }
-        _ => (),
-    }
-}
-
 #[allow(unused_must_use)] // this is exit anyway
-fn exit_app(terminal: &mut Terminal<CrosstermBackend<Stdout>>) {
-    terminal.clear();
-    terminal.set_cursor(0, 0);
-    terminal.show_cursor();
+fn exit_app() {
+    execute!(io::stdout(), LeaveAlternateScreen);
     exit(0)
 }
