0a063a2a2443ec300d94ee89c14fe99c52c8c852
Artem Pyanykh <artem.pyanykh@gmail.com>
2021-02-10 14:27:53 +0000

    Use win-style paths in include_str! when building on Windows

--- src/plug.rs
+++ src/plug.rs
@@ -2,12 +2,23 @@ use crate::shell::{self, OutputType};
 pub struct PlugCommand {}
 
 impl shell::Output for PlugCommand {
+    #[cfg(not(target_os = "windows"))]
     fn to_output(&self, out_type: OutputType) -> String {
         match out_type {
             OutputType::Plain => String::new(),
             OutputType::Fish => include_str!("../integration/s.fish").to_string(),
             OutputType::PowerShell => include_str!("../integration/s.ps1").to_string(),
             OutputType::Posix => include_str!("../integration/s.sh").to_string(),
+        }
+    }
+
+    #[cfg(target_os = "windows")]
+    fn to_output(&self, out_type: OutputType) -> String {
+        match out_type {
+            OutputType::Plain => String::new(),
+            OutputType::Fish => include_str!("..\\integration\\s.fish").to_string(),
+            OutputType::PowerShell => include_str!("..\\integration\\s.ps1").to_string(),
+            OutputType::Posix => include_str!("..\\integration\\s.sh").to_string(),
         }
     }
 }
